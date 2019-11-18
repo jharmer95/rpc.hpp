955b74cafcd472f5aeb3bc223df35746b853a126
Jackson Harmer <jackson.harmer@gm.com>
2019-11-18 10:18:46 -0500

    - Some cleanup from linting

--- include/rpc.hpp
+++ include/rpc.hpp
@@ -28,15 +28,15 @@ struct function_traits;
 template<typename R, typename... Args>
 struct function_traits<std::function<R(Args...)>>
 {
-	static constexpr size_t nargs = sizeof...(Args);
+    static constexpr size_t nargs = sizeof...(Args);
 
-	using result_type = R;
+    using result_type = R;
 
-	template<size_t i>
-	struct arg
-	{
-		using type = typename std::tuple_element<i, std::tuple<Args...>>::type;
-	};
+    template<size_t i>
+    struct arg
+    {
+        using type = typename std::tuple_element<i, std::tuple<Args...>>::type;
+    };
 };
 
 template<typename R, typename... Args>
@@ -53,222 +53,223 @@ namespace rpc
 template<typename, typename T>
 struct is_serializable
 {
-	static_assert(std::integral_constant<T, false>::value,
-		"Second template parameter needs to be of function type");
+    static_assert(std::integral_constant<T, false>::value,
+        "Second template parameter needs to be of function type");
 };
 
 template<typename C, typename R, typename... Args>
 struct is_serializable<C, R(Args...)>
 {
 private:
-	template<typename T>
-	static constexpr auto check(T*) ->
-		typename std::is_same<decltype(std::declval<T>().Serialize(std::declval<Args>()...)),
-			R>::type;
+    template<typename T>
+    static constexpr auto check(T*) ->
+        typename std::is_same<decltype(std::declval<T>().Serialize(std::declval<Args>()...)),
+            R>::type;
 
-	template<typename>
-	static constexpr std::false_type check(...);
+    template<typename>
+    static constexpr std::false_type check(...);
 
-	typedef decltype(check<C>(0)) type;
+    using type = decltype(check<C>(nullptr));
 
 public:
-	static constexpr bool value = type::value;
+    static constexpr bool value = type::value;
 };
 
 template<typename T>
+[[nodiscard]]
 T DecodeArgArray(const njson::json& obj_j, uint8_t* buf, size_t* count)
 {
-	*count = 1UL;
+    *count = 1UL;
 
-	if (obj_j.is_array())
-	{
-		// Multi-value pointer (array)
+    if (obj_j.is_array())
+    {
+        // Multi-value pointer (array)
 
-		// TODO: Also support containers
+        // TODO: Also support containers
+        using P = std::remove_pointer_t<T>;
+        static_assert(!std::is_void_v<P>,
+            "Void pointers are not supported, either cast to a different type or do the conversion "
+            "manually!");
 
-		using P = std::remove_pointer_t<T>;
-		static_assert(!std::is_void_v<P>,
-			"Void pointers are not supported, either cast to a different type or do the conversion "
-			"manually!");
+        const auto bufPtr = reinterpret_cast<T>(buf);
 
-		T bufPtr = reinterpret_cast<T>(buf);
+        if constexpr (is_serializable<P, njson::json(const P&)>::value)
+        {
+            const auto values = P::DeSerialize(obj_j);
 
-		if constexpr (is_serializable<P, njson::json(const P&)>::value)
-		{
-			const auto values = P::DeSerialize(obj_j);
+            if (values.size() != obj_j.size())
+            {
+                throw std::runtime_error(
+                    "JSON object was not deserialized correctly: number of arguments changed");
+            }
 
-			if (values.size() != obj_j.size())
-			{
-				throw std::runtime_error(
-					"JSON object was not deserialized correctly: number of arguments changed");
-			}
+            std::copy(values.begin(), values.end(), bufPtr);
+        }
+        else if constexpr (std::is_arithmetic_v<P> || std::is_same_v<P, std::string>)
+        {
+            for (size_t i = 0; i < obj_j.size(); ++i)
+            {
+                bufPtr[i] = obj_j[i].get<P>();
+            }
+        }
 
-			std::copy(values.begin(), values.end(), bufPtr);
-		}
-		else if constexpr (std::is_arithmetic_v<P> || std::is_same_v<P, std::string>)
-		{
-			for (size_t i = 0; i < obj_j.size(); ++i)
-			{
-				bufPtr[i] = obj_j[i].get<P>();
-			}
-		}
+        *count = obj_j.size();
+        return bufPtr;
+    }
 
-		*count = obj_j.size();
-		return bufPtr;
-	}
+    // Single value pointer
+    using P = std::remove_pointer_t<T>;
+    static_assert(!std::is_void_v<P>,
+        "Void pointers are not supported, either cast to a different type or do the conversion "
+        "manually!");
 
-	// Single value pointer
-	using P = std::remove_pointer_t<T>;
-	static_assert(!std::is_void_v<P>,
-		"Void pointers are not supported, either cast to a different type or do the conversion "
-		"manually!");
+    const auto bufPtr = reinterpret_cast<T>(buf);
 
-	const T bufPtr = reinterpret_cast<T>(buf);
+    if constexpr (is_serializable<P, njson::json(const P&)>::value)
+    {
+        const auto value = P::DeSerialize(obj_j)[0];
+        *bufPtr = value;
+    }
+    else if constexpr (std::is_same_v<P, char>)
+    {
+        const auto str = obj_j.get<std::string>();
+        std::copy(str.begin(), str.end(), bufPtr);
+    }
+    else if constexpr (std::is_arithmetic_v<P> || std::is_same_v<P, std::string>)
+    {
+        const auto value = obj_j.get<P>();
+        *bufPtr = value;
+    }
 
-	if constexpr (is_serializable<P, njson::json(const P&)>::value)
-	{
-		const auto value = P::DeSerialize(obj_j)[0];
-		*bufPtr = value;
-	}
-	else if constexpr (std::is_same_v<P, char>)
-	{
-		const auto str = obj_j.get<std::string>();
-		std::copy(str.begin(), str.end(), bufPtr);
-	}
-	else if constexpr (std::is_arithmetic_v<P> || std::is_same_v<P, std::string>)
-	{
-		const auto value = obj_j.get<P>();
-		*bufPtr = value;
-	}
-
-	return bufPtr;
+    return bufPtr;
 }
 
 template<typename T>
+[[nodiscard]]
 T DecodeArg(const njson::json& obj_j, uint8_t* buf, size_t* count, unsigned* paramNum)
 {
-	*paramNum += 1;
-	*count = 1UL;
+    *paramNum += 1;
+    *count = 1UL;
 
-	if constexpr (std::is_pointer_v<T>)
-	{
-		return DecodeArgArray<T>(obj_j, buf, count);
-	}
-	else if constexpr (is_serializable<T, njson::json(const T&)>::value)
-	{
-		return T::DeSerialize(obj_j)[0];
-	}
-	else if constexpr (std::is_arithmetic_v<T> || std::is_same_v<T, std::string>)
-	{
-		T retVal = obj_j.get<T>();
-		return retVal;
-	}
-	else
-	{
-		throw std::runtime_error("Invalid type!");
-	}
+    if constexpr (std::is_pointer_v<T>)
+    {
+        return DecodeArgArray<T>(obj_j, buf, count);
+    }
+    else if constexpr (is_serializable<T, njson::json(const T&)>::value)
+    {
+        return T::DeSerialize(obj_j)[0];
+    }
+    else if constexpr (std::is_arithmetic_v<T> || std::is_same_v<T, std::string>)
+    {
+        T retVal = obj_j.get<T>();
+        return retVal;
+    }
+    else
+    {
+        throw std::runtime_error("Invalid type!");
+    }
 }
 
 template<typename T>
 void EncodeArgs(njson::json& args_j, const size_t count, const T& val)
 {
-	if constexpr (std::is_pointer_v<T>)
-	{
-		using P = std::remove_pointer_t<T>;
+    if constexpr (std::is_pointer_v<T>)
+    {
+        using P = std::remove_pointer_t<T>;
 
-		for (size_t i = 0; i < count; ++i)
-		{
-			if constexpr (is_serializable<P, njson::json(const P&)>::value)
-			{
-				args_j.push_back(P::Serialize(val[i]));
-			}
-			else if constexpr (std::is_same_v<P, char>)
-			{
-				args_j.push_back(std::string(val, count));
-			}
-			else if constexpr (std::is_arithmetic_v<P> || std::is_same_v<P, std::string>)
-			{
-				args_j.push_back(val[i]);
-			}
-		}
-	}
-	else
-	{
-		if constexpr (is_serializable<T, njson::json(const T&)>::value)
-		{
-			args_j.push_back(T::Serialize(val));
-		}
-		else if constexpr (std::is_arithmetic_v<T> || std::is_same_v<T, std::string>)
-		{
-			args_j.push_back(val);
-		}
-	}
+        for (size_t i = 0; i < count; ++i)
+        {
+            if constexpr (is_serializable<P, njson::json(const P&)>::value)
+            {
+                args_j.push_back(P::Serialize(val[i]));
+            }
+            else if constexpr (std::is_same_v<P, char>)
+            {
+                args_j.push_back(std::string(val, count));
+            }
+            else if constexpr (std::is_arithmetic_v<P> || std::is_same_v<P, std::string>)
+            {
+                args_j.push_back(val[i]);
+            }
+        }
+    }
+    else
+    {
+        if constexpr (is_serializable<T, njson::json(const T&)>::value)
+        {
+            args_j.push_back(T::Serialize(val));
+        }
+        else if constexpr (std::is_arithmetic_v<T> || std::is_same_v<T, std::string>)
+        {
+            args_j.push_back(val);
+        }
+    }
 }
 
 template<typename F, typename... Ts, size_t... Is>
 void for_each_tuple(const std::tuple<Ts...>& tuple, F func, std::index_sequence<Is...>)
 {
-	using expander = int[];
-	(void)expander{ 0, ((void)func(std::get<Is>(tuple)), 0)... };
+    using expander = int[];
+    (void)expander{ 0, ((void)func(std::get<Is>(tuple)), 0)... };
 }
 
 template<typename F, typename... Ts>
 void for_each_tuple(const std::tuple<Ts...>& tuple, F func)
 {
-	for_each_tuple(tuple, func, std::make_index_sequence<sizeof...(Ts)>());
+    for_each_tuple(tuple, func, std::make_index_sequence<sizeof...(Ts)>());
 }
 
 template<typename R, typename... Args>
+[[nodiscard]]
 std::string RunCallBack(const njson::json& obj_j, std::function<R(Args...)> func)
 {
-	unsigned count = 0;
+    unsigned count = 0;
 
-	std::array<std::pair<size_t, std::unique_ptr<unsigned char[]>>,
-		function_param_count_v<R, Args...>>
-		buffers;
+    std::array<std::pair<size_t, std::unique_ptr<unsigned char[]>>,
+        function_param_count_v<R, Args...>>
+        buffers;
 
-	for (auto& buf : buffers)
-	{
-		buf.first = 0UL;
-		buf.second = std::make_unique<unsigned char[]>(4096);
-	}
+    for (auto& buf : buffers)
+    {
+        buf.first = 0UL;
+        buf.second = std::make_unique<unsigned char[]>(4096);
+    }
 
-	const std::string dbg = obj_j[count + 1].dump();
+    std::tuple<Args...> args{ DecodeArg<Args>(
+        obj_j[count], buffers[count].second.get(), &(buffers[count].first), &count)... };
 
-	std::tuple<Args...> args{ DecodeArg<Args>(
-		obj_j[count], buffers[count].second.get(), &(buffers[count].first), &count)... };
+    const auto result = std::apply(func, args);
 
-	const auto result = std::apply(func, args);
+    njson::json retObj_j;
 
-	njson::json retObj_j;
+    retObj_j["result"] = result;
+    retObj_j["args"] = njson::json::array();
+    auto& argList = retObj_j["args"];
 
-	retObj_j["result"] = result;
-	retObj_j["args"] = njson::json::array();
-	auto& argList = retObj_j["args"];
+    unsigned count2 = 0;
 
-	unsigned count2 = 0;
+    for_each_tuple(args, [&argList, &buffers, &count2](const auto& x) {
+        EncodeArgs(argList, buffers[count2++].first, x);
+    });
 
-	for_each_tuple(args, [&argList, &buffers, &count2](const auto& x) {
-		EncodeArgs(argList, buffers[count2++].first, x);
-	});
-
-	return retObj_j.dump();
+    return retObj_j.dump();
 }
 
 template<typename TDispatcher>
-inline std::string RunFromJSON(const njson::json& obj_j, const TDispatcher& dispatch)
+[[nodiscard]]
+std::string RunFromJSON(const njson::json& obj_j, const TDispatcher& dispatch)
 {
-	const auto funcName = obj_j["function"].get<std::string>();
-	const auto& argList = obj_j["args"];
+    const auto funcName = obj_j["function"].get<std::string>();
+    const auto& argList = obj_j["args"];
 
-	try
-	{
-		return RunCallBack(argList, dispatch.get(funcName));
-	}
-	catch (std::exception& ex)
-	{
-		std::cerr << ex.what() << '\n';
-		return "{ \"result\": -1 }";
-	}
+    try
+    {
+        return RunCallBack(argList, dispatch.get(funcName));
+    }
+    catch (std::exception& ex)
+    {
+        std::cerr << ex.what() << '\n';
+        return "{ \"result\": -1 }";
+    }
 }
 } // namespace rpc
--- tests/main.cpp
+++ tests/main.cpp
@@ -19,96 +19,97 @@
 
 struct TestStruct
 {
-	int age;
-	char name[255]{};
-	int sector;
-	unsigned long userID;
+    int age{};
+    int sector{};
+    unsigned long userID{};
+    char name[255]{};
 
-	// TODO: Get templated Serialize/Deserialize working
-	static nlohmann::json Serialize(const TestStruct& st)
-	{
-		nlohmann::json obj_j;
-		obj_j["age"] = st.age;
-		obj_j["name"] = std::string(st.name);
-		obj_j["sector"] = st.sector;
-		obj_j["userID"] = st.userID;
+    // TODO: Get templated Serialize/Deserialize working
+    static nlohmann::json Serialize(const TestStruct& st)
+    {
+        nlohmann::json obj_j;
+        obj_j["age"] = st.age;
+        obj_j["name"] = std::string(st.name);
+        obj_j["sector"] = st.sector;
+        obj_j["userID"] = st.userID;
 
-		return obj_j;
-	}
+        return obj_j;
+    }
 
-	// TODO: move array stuff to the library side so that return type is just TestStruct
-	static std::vector<TestStruct> DeSerialize(const nlohmann::json& obj_j)
-	{
-		std::vector<TestStruct> vec;
-		const size_t sz = obj_j.is_array() ? obj_j.size() : 1;
+    // TODO: move array stuff to the library side so that return type is just TestStruct
+    static std::vector<TestStruct> DeSerialize(const nlohmann::json& obj_j)
+    {
+        std::vector<TestStruct> vec;
+        const size_t sz = obj_j.is_array() ? obj_j.size() : 1;
 
-		for (size_t i = 0; i < sz; ++i)
-		{
-			TestStruct ts;
-			ts.age = obj_j["age"].get<int>();
-			const auto nmStr = obj_j["name"].get<std::string>();
-			std::copy(nmStr.begin(), nmStr.end(), ts.name);
-			ts.sector = obj_j["sector"].get<int>();
-			ts.userID = obj_j["userID"].get<unsigned long>();
-			vec.push_back(ts);
-		}
+        for (size_t i = 0; i < sz; ++i)
+        {
+            TestStruct ts;
+            ts.age = obj_j["age"].get<int>();
+            const auto nmStr = obj_j["name"].get<std::string>();
+            std::copy(nmStr.begin(), nmStr.end(), ts.name);
+            ts.sector = obj_j["sector"].get<int>();
+            ts.userID = obj_j["userID"].get<unsigned long>();
+            vec.push_back(ts);
+        }
 
-		return vec;
-	}
+        return vec;
+    }
 };
 
-int PrintMyArgs(TestStruct* pts, int n, std::string msg)
+int PrintMyArgs(TestStruct* pts, const int n, const std::string& msg)
 {
-	std::cout << "age: " << pts->age << '\n';
-	std::cout << "name: " << pts->name << '\n';
-	std::cout << "sector: " << pts->sector << '\n';
-	std::cout << "userID: " << pts->userID << "\n\n";
-	std::cout << "n: " << n << '\n';
-	std::cout << "msg: " << msg << '\n';
+    std::cout << "age: " << pts->age << '\n';
+    std::cout << "name: " << pts->name << '\n';
+    std::cout << "sector: " << pts->sector << '\n';
+    std::cout << "userID: " << pts->userID << "\n\n";
+    std::cout << "n: " << n << '\n';
+    std::cout << "msg: " << msg << '\n';
 
-	return 2;
+    return 2;
 }
 
 class Dispatcher
 {
 public:
-	auto get(const std::string& sv) const
-	{
-		if (sv == "PrintMyArgs")
-		{
-			return m_printArgs;
-		}
+    [[nodiscard]]
+    auto get(const std::string& sv) const
+    {
+        if (sv == "PrintMyArgs")
+        {
+            return m_printArgs;
+        }
 
-		throw std::runtime_error("Could not find function!");
-	}
+        throw std::runtime_error("Could not find function!");
+    }
 
 private:
-	std::function<int(TestStruct*, int n, std::string)> m_printArgs = PrintMyArgs;
-} myDispatch;
+    std::function<int(TestStruct*, int n, std::string)> m_printArgs = PrintMyArgs;
+} myDispatch{};
 
 int main()
 {
-	nlohmann::json send_j;
+    nlohmann::json send_j;
 
-	send_j["args"] = nlohmann::json::array();
-	send_j["function"] = "PrintMyArgs";
-	auto& argList = send_j["args"];
+    send_j["args"] = nlohmann::json::array();
+    send_j["function"] = "PrintMyArgs";
+    auto& argList = send_j["args"];
 
-	TestStruct ts;
+    TestStruct ts;
 
-	ts.age = 5;
-	std::string nmStr = "Frank Tank";
-	std::copy(nmStr.begin(), nmStr.end(), ts.name);
-	ts.sector = 5545;
-	ts.userID = 12345678UL;
+    ts.age = 5;
+    std::string nmStr = "Frank Tank";
+    std::copy(nmStr.begin(), nmStr.end(), ts.name);
+    ts.sector = 5545;
+    ts.userID = 12345678UL;
 
-	argList.push_back(TestStruct::Serialize(ts));
-	argList.push_back(45);
-	argList.push_back("Hello world!");
+    argList.push_back(TestStruct::Serialize(ts));
+    argList.push_back(45);
+    argList.push_back("Hello world!");
 
-	const auto retMsg = rpc::RunFromJSON(send_j, myDispatch);
+    const auto retMsg = rpc::RunFromJSON(send_j, myDispatch);
 
-	std::cout << "\nReturn message:\n" << retMsg << '\n';
+    std::cout << "\nReturn message:\n" << retMsg << '\n';
 
-	return 0;
+    return 0;
 }
