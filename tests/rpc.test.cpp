8fb591765a1e2a34ea2b973c9374c818a053c30c
Jackson <jharmer95@gmail.com>
2021-03-01 21:47:45 -0500

    Update rpc.test.cpp

--- tests/rpc.test.cpp
+++ tests/rpc.test.cpp
@@ -421,7 +421,7 @@ TEST_CASE("HashComplexRef")
     REQUIRE_THAT(expected, Catch::Matchers::Equals(test));
 }
 
-TEST_CASE("KillServer")
+TEST_CASE("KillServer", "[!mayfail])
 {
     auto& client = GetClient<test_serial_t>();
 
